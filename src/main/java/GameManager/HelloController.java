470b2f09aa95e8088a06ae9bf399814288cfbaa7
24020196 <24020196@vnu.edu.vn>
2025-10-15 12:57:39 +0700

    test controler

--- src/main/java/GameManager/HelloController.java
+++ src/main/java/GameManager/HelloController.java
@@ -6,13 +6,15 @@ import RenderView.Menu;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 import static javafx.application.Application.launch;
 
 public class HelloController {
     @FXML
     Button btn;
-
+    @FXML
+    VBox vbox;
     public void initialize() {
         GameObject gameObject= new Brick(10,10,100,200,1,1);
         btn.setPrefSize(gameObject.getWidth(),gameObject.getHeight());
@@ -20,6 +22,9 @@ public class HelloController {
     }
 
     private void setupEvents() {
+        vbox.setOnMouseMoved(mouseEvent -> {
+            System.out.println(mouseEvent.getX() + " " + mouseEvent.getY());
+        });
         btn.setOnAction(event -> {
             System.out.println("Brick clicked!");
 
