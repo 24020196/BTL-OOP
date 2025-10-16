8e65cb8cd03659ad81c242f053c4f517dadd1bf5
24020196 <24020196@vnu.edu.vn>
2025-10-16 16:47:27 +0700

    update menu button

--- src/main/java/GameManager/MenuController.java
+++ src/main/java/GameManager/MenuController.java
@@ -3,7 +3,9 @@ package GameManager;
 import Entity.Brick;
 import Entity.GameObject;
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
@@ -17,10 +19,19 @@ public class MenuController {
     ImageView menuBackground;
     @FXML
     ImageView btnPlayGame;
+    @FXML
+    ImageView btnHighScores;
+    @FXML
+    ImageView btnSetting;
+    @FXML
+    ImageView btnExit;
 
     public void initialize() {
-        btnPlayGame.setVisible(false);
-
+        resizeImageView(menuBackground, 0 , 0 , 1280 , 720);
+        resizeImageView(btnPlayGame, 356, 207, 495, 106);
+        resizeImageView(btnHighScores, 356, 336, 495, 106);
+        resizeImageView(btnSetting, 356, 462, 495, 106);
+        resizeImageView(btnExit, 356, 590, 495, 106);
         menuLayoutEvents();
     }
 
@@ -29,31 +40,49 @@ public class MenuController {
             System.out.println(mouseEvent.getX() + " " + mouseEvent.getY());
         });
 
+        menuLayout.layoutBoundsProperty().addListener((obs, oldBounds, newBounds) -> {
+            resizeImageView(menuBackground, 0 , 0 , 1280 , 720 , newBounds);
+            resizeImageView(btnPlayGame, 356, 207, 495, 106, newBounds);
+            resizeImageView(btnHighScores, 356, 336, 495, 106, newBounds);
+            resizeImageView(btnSetting, 356, 462, 495, 106, newBounds);
+            resizeImageView(btnExit, 356, 590, 495, 106, newBounds);
+        });
+
         menuLayout.setOnMouseMoved( mouseMove -> {
-            btnPlayGame.setVisible(false);
-            if(mouseMove.getX() >= btnPlayGame.getLayoutX() && mouseMove.getX() <= btnPlayGame.getLayoutX()+btnPlayGame.getFitWidth())
-                if (mouseMove.getY() >= btnPlayGame.getLayoutY() && mouseMove.getY() <= btnPlayGame.getLayoutY()+btnPlayGame.getFitHeight())
-                    btnPlayGame.setVisible(true);
-        });
+            setVisibleImageView(btnPlayGame, mouseMove.getX(), mouseMove.getY());
+            setVisibleImageView(btnHighScores, mouseMove.getX(), mouseMove.getY());
+            setVisibleImageView(btnSetting, mouseMove.getX(), mouseMove.getY());
+            setVisibleImageView(btnExit, mouseMove.getX(), mouseMove.getY());
 
-        menuLayout.widthProperty().addListener((obs, oldWidth, newWidth) -> {
-            menuBackground.fitWidthProperty().bind(menuLayout.widthProperty());
-
-            btnPlayGame.setLayoutX(356 * newWidth.doubleValue() / 1280);
-            btnPlayGame.setFitWidth(495 * newWidth.doubleValue() / 1280);
-
-        });
-
-        menuLayout.heightProperty().addListener((obs, oldHeight, newHeight) -> {
-            menuBackground.fitHeightProperty().bind(menuLayout.heightProperty());
-
-            btnPlayGame.setLayoutY(207 * newHeight.doubleValue() / 720);
-            btnPlayGame.setFitHeight(106 * newHeight.doubleValue() / 720);
         });
 
         btnPlayGame.setOnMouseClicked(event -> {
             System.out.println("Brick clicked!");
         });
+    }
+
+    void resizeImageView(ImageView imageView, double x, double y, double width, double height) {
+        imageView.setLayoutX(x);
+        imageView.setLayoutY(y);
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+    }
+
+    void resizeImageView(ImageView imageView, double x, double y, double width, double height, Bounds bounds) {
+        imageView.setLayoutX(x * bounds.getWidth() / 1280);
+        imageView.setLayoutY(y *  bounds.getHeight() / 720);
+        imageView.setFitWidth(width * bounds.getWidth() / 1280);
+        imageView.setFitHeight(height*  bounds.getHeight() / 720);
+    }
+
+    void setVisibleImageView(ImageView imageView, double x, double y) {
+        double left = imageView.getLayoutX();
+        double right = imageView.getLayoutX() + imageView.getFitWidth();
+        double up = imageView.getLayoutY();
+        double down = imageView.getLayoutY() + imageView.getFitHeight();
+        imageView.setVisible(false);
+        if(x >= left && x <= right)
+            if(y >= up & y <= down) imageView.setVisible(true);
     }
 
     public void startGame() {
--- src/main/java/RenderView/Menu.java
+++ src/main/java/RenderView/Menu.java
@@ -12,13 +12,11 @@ public class Menu extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Menu.class.getResource("menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
