0553ae34201a6c1e3ec33cb4285f068d6015fd68
kkk15-vn <khangnguyen162006@gmail.com>
2025-10-30 23:07:41 +0700

    dat->khang
    khung của đạt, sửa đổi của khang

---
+++ src/main/java/GameDatabase/DatabaseConnection.java
@@ -0,0 +1,26 @@
+package GameDatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://10.11.76.77:3306/breakout_game"; // thay game_db bằng tên DB của bạn
+    private static final String USER = "root"; // user MySQL
+    private static final String PASSWORD = "admin"; // password MySQL của bạn
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println(" Kết nối MySQL thành công!");
+            } catch (SQLException e) {
+                System.out.println(" Lỗi kết nối MySQL: " + e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+}
---
+++ src/main/java/GameManager/LevelMapController.java
@@ -0,0 +1,94 @@
+package GameManager;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class LevelMapController {
+    @FXML AnchorPane levelLayout;
+    @FXML ImageView levelMapBackground;
+    @FXML ImageView level1;
+    @FXML ImageView level2;
+    @FXML ImageView level3;
+    @FXML ImageView level4;
+    @FXML ImageView level5;
+    @FXML ImageView level6;
+    @FXML ImageView level7;
+    @FXML ImageView level8;
+    @FXML ImageView level9;
+
+
+    public void initialize() {
+        setsize();
+        levelLayoutEvents();
+    }
+
+    private void levelLayoutEvents() {
+        levelLayout.setOnMouseClicked(mouseEvent -> {
+            System.out.println(mouseEvent.getX() + " " + mouseEvent.getY());
+        });
+
+        levelLayout.setOnMouseMoved( mouseMove -> {
+
+
+        });
+
+        ImageView[] levels = {level1, level2, level3, level4, level5, level6, level7, level8, level9};
+
+        for (int i = 0; i < levels.length; i++) {
+            final int levelIndex = i + 1;
+            levels[i].setOnMouseClicked(event -> {
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/RenderView/game.fxml"));
+                    Parent root = loader.load();
+                    Stage stage = (Stage) levelLayout.getScene().getWindow();
+                    stage.setScene(new Scene(root));
+                    stage.show();
+                    System.out.println("Loading level " + levelIndex);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+
+    void setsize() {
+
+        update(levelMapBackground,0,0,1280,720);
+        update(level1,50,100,180,125);
+        update(level2,50,100,180,125);
+        update(level3,50,100,180,125);
+        update(level4,50,100,180,125);
+        update(level5,50,100,180,125);
+        update(level6,50,100,180,125);
+        update(level7,50,100,180,125);
+        update(level8,50,100,180,125);
+        update(level9,50,100,180,125);
+    }
+
+    void update(ImageView imageView, double x, double y, double width, double height) {
+        imageView.setLayoutX(x);
+        imageView.setLayoutY(y);
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+    }
+
+    void setVisibleImageView(ImageView imageView, double x, double y) {
+        double left = imageView.getLayoutX();
+        double right = imageView.getLayoutX() + imageView.getFitWidth();
+        double up = imageView.getLayoutY();
+        double down = imageView.getLayoutY() + imageView.getFitHeight();
+        imageView.setVisible(false);
+        if(x >= left && x <= right)
+            if(y >= up & y <= down) imageView.setVisible(true);
+    }
+
+}
